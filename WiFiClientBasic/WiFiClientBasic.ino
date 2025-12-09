eff2c237929bac2d9a1afc663069c3996c4134a6
unknown <sandesh4080@gmail.com>
2025-12-09 21:59:22 +0530

    removed comments

--- WiFiClientBasic/WiFiClientBasic.ino
+++ WiFiClientBasic/WiFiClientBasic.ino
@@ -1,8 +1,3 @@
-/*
- *  This sketch sends a message to a TCP server
- *
- */
-
 #include <WiFi.h>
 #include <WiFiMulti.h>
 
@@ -12,7 +7,6 @@ void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // We start by connecting to a WiFi network
   WiFiMulti.addAP("KCH40", "123456788");
 
   Serial.println();
@@ -33,15 +27,12 @@ void setup() {
 }
 
 void loop() {
-  //    const uint16_t port = 80;
-  //    const char * host = "192.168.1.1"; // ip or dns
   const uint16_t port = 1337;
   const char *host = "192.168.1.10";  // ip or dns
 
   Serial.print("Connecting to ");
   Serial.println(host);
 
-  // Use NetworkClient class to create TCP connections
   NetworkClient client;
 
   if (!client.connect(host, port)) {
@@ -51,21 +42,15 @@ void loop() {
     return;
   }
 
-  // This will send a request to the server
-  //uncomment this line to send an arbitrary string to the server
-  //client.print("Send this data to the server");
-  //uncomment this line to send a basic document request to the server
   client.print("GET /index.html HTTP/1.1\n\n");
 
   int maxloops = 0;
 
-  //wait for the server's reply to become available
   while (!client.available() && maxloops < 1000) {
     maxloops++;
     delay(1);  //delay 1 msec
   }
   if (client.available() > 0) {
-    //read back one line from the server
     String line = client.readStringUntil('\r');
     Serial.println(line);
   } else {
