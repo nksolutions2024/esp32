2e81f9cea7c23fca15de4a3eace4ced06cc7a6fd
unknown <sandesh4080@gmail.com>
2025-12-09 22:02:10 +0530

    reduce code

--- WiFiClientBasic/WiFiClientBasic.ino
+++ WiFiClientBasic/WiFiClientBasic.ino
@@ -1,65 +1,40 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 
-WiFiMulti WiFiMulti;
+WiFiMulti wifiMulti;
 
 void setup() {
-  Serial.begin(115200);
-  delay(10);
+  Serial.begin(11520);
+  wifiMulti.addAP("KCH40", "123456788");
 
-  WiFiMulti.addAP("KCH40", "123456788");
-
-  Serial.println();
-  Serial.println();
-  Serial.print("Waiting for WiFi... ");
-
-  while (WiFiMulti.run() != WL_CONNECTED) {
-    Serial.print(".");
+  while (wifiMulti.run() != WL_CONNECTED) {
     delay(500);
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println("WiFi Connected");
   Serial.println(WiFi.localIP());
-
-  delay(500);
 }
 
 void loop() {
-  const uint16_t port = 1337;
-  const char *host = "192.168.1.10";  // ip or dns
+  WiFiClient client;
 
-  Serial.print("Connecting to ");
-  Serial.println(host);
-
-  NetworkClient client;
-
-  if (!client.connect(host, port)) {
-    Serial.println("Connection failed.");
-    Serial.println("Waiting 5 seconds before retrying...");
-    delay(5000);
+  if (!client.connect("192.168.1.10", 1337)) {
+    Serial.println("Connect fail");
+    delay(3000);
     return;
   }
 
-  client.print("GET /index.html HTTP/1.1\n\n");
+  client.println("Hello Server");
 
-  int maxloops = 0;
+  unsigned long t = millis();
+  while (!client.available() && millis() - t < 2000) delay(10);
 
-  while (!client.available() && maxloops < 1000) {
-    maxloops++;
-    delay(1);  //delay 1 msec
-  }
-  if (client.available() > 0) {
-    String line = client.readStringUntil('\r');
-    Serial.println(line);
+  if (client.available()) {
+    Serial.println(client.readString());
   } else {
-    Serial.println("client.available() timed out ");
+    Serial.println("No data");
   }
 
-  Serial.println("Closing connection.");
   client.stop();
-
-  Serial.println("Waiting 5 seconds before restarting...");
-  delay(5000);
+  delay(3000);
 }
