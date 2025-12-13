51713d270bd4105df7d1d7b72d834eeccfd61217
kite <sandesh4080@gmail.com>
2025-12-13 16:57:13 +0530

    testing

--- WiFiClientBasic/WiFiClientBasic.ino
+++ WiFiClientBasic/WiFiClientBasic.ino
@@ -2,9 +2,12 @@
 #include <WiFiMulti.h>
 
 WiFiMulti wifiMulti;
+int counter = 1;
+#define LED_BUILTIN 2
 
 void setup() {
-  Serial.begin(11520);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
   wifiMulti.addAP("KCH40", "123456788");
 
   while (wifiMulti.run() != WL_CONNECTED) {
@@ -16,6 +19,12 @@ void setup() {
 }
 
 void loop() {
+  Serial.println(++counter);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(80);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(80);
+
   WiFiClient client;
 
   if (!client.connect("192.168.1.10", 1337)) {
