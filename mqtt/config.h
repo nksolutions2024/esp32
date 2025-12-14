a09fd745934f518493bdc4c22fb3d3eaff059c6a
kite <sandesh4080@gmail.com>
2025-12-14 08:53:17 +0530

    step3 mqtt

--- mqtt/config.h
+++ mqtt/config.h
@@ -2,20 +2,20 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* WIFI_SSID = "iot2";
-password ;
+const char* WIFI_SSID = "KCH40";
+const char* WIFI_PASSWORD = "123456788" ;
 
-CHAR* MOTT_BROKER = "13.233.232.181";
-INT MQTT_PORT = 1883;
-CHAR* MQTT_CLIENT_ID  = "IOTNODE-01";
+const char* MOTT_BROKER = "13.233.232.181";
+const int MQTT_PORT = 1883;
+const char* MQTT_CLIENT_ID  = "iotnode-08";
 
-CHAR* MQTT_USERNAME = "DESD";
-CHAR* MQTT_PASSWORD = "DESD123";
+const char* MQTT_USERNAME = "desd8";
+const char* MQTT_PASSWORD = "desd1234";
 
 
-C* MQTT_TOPIC_PUBLISH = "CDAC/DIOT/TELEMETRY"
-	MQTT_TOPIC_SUBSCRIBE = "CDAC/DIOT/LED/CONTROL"
+const char* MQTT_TOPIC_PUBLISH = "cdac/diot/telemetry";
+const char* MQTT_TOPIC_SUBSCRIBE = "cdac/diot/led/control";
 
-const uint8_t NEW_MAC_ADDRESS[] = {0XDC, 0x1b, 0x64, 0x5e, 0xbf};
+const uint8_t NEW_MAC_ADDRESS[] = {0xdc, 0x1b, 0x64, 0x5e, 0xbf};
 
-#ENDIF
+#endif
